eedf740724b780cfbbf474da17bad7f76f9d959d
MichaelKasingye <michaelkasingye@gmail.com>
2021-10-18 10:24:31 +0300

    new project

---
+++ Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+function Navbar() {
+    return (
+        <h4 className={styles.title}>
+         <a href="#">Corhort 2 blog</a>
+      </h4>
+    )
+}
+
+export default Navbar
